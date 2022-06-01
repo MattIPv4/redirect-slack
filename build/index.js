ea530c960be5f59a1a16fde36839a375d21af593
MattIPv4 <me@mattcowley.co.uk>
2022-06-01 13:58:18 +0100

    Fix invite generation

--- build/index.js
+++ build/index.js
@@ -9,7 +9,7 @@ const main = async () => {
     // Create the form
     const form = new FormData();
     form.append('expiration', '99999');
-    form.append('max_signups', '99999');
+    form.append('max_signups', '100');
     form.append('set_active', 'true');
     form.append('token', process.env.SLACK_USER_TOKEN);
 
@@ -21,7 +21,11 @@ const main = async () => {
             body: form,
         },
     );
+
+    // Get the response
+    if (!resp.ok) throw new Error(`Slack API returned ${resp.status}: ${resp.statusText}\n${await resp.text().catch(() => '')}`);
     const data = await resp.json();
+    if (!data.ok) throw new Error(`Slack API returned error: ${data.error}`);
 
     // Generate the new files
     const template = await fs.readFile(path.join(__dirname, 'template.html'), 'utf8');
